88e94eef10677f51c67bd5d23b9d0c430c57165b
Lisa Kim <lisakeem@outlook.com>
2018-11-12 13:36:36 -0800

    implement func LogRequestService

--- logtag/logtag.go
+++ logtag/logtag.go
@@ -1,8 +1,14 @@
 package logtag
 
+import "log"
+
 const (
 	Debug = "[DEBUG]"
 	Info = "[INFO]"
 	Error = "[ERROR]"
 	Fatal = "[FATAL]"
 )
+
+func LogRequestService (svc string) {
+	log.Printf("%s Requesting %s service", Info, svc)
+}
