f49b4e292baee2a0f778e9bf00af6b8409d5c6da
CJ Faraon <faraonc@outlook.com>
2019-02-09 19:21:31 -0800

    Implement utility functions for Authentication/Authorization

---
+++ auth/auth.go
@@ -0,0 +1,23 @@
+package auth
+
+// Authorize the given Token using a Secret
+func Authorize() {
+
+}
+
+// Invalidate the Token
+func Invalidate() {
+
+}
+
+// HasExpired checks if the Token has expired
+func HasExpired() bool {
+
+	return false
+}
+
+// IsAuthorized checks if the Token is authorized using a Secret
+func IsAuthorized() bool {
+
+	return true
+}
