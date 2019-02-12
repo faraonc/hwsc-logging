8c4b145c4dd42d868aab431c6543465497dfb152
faraonc <faraonc@outlook.com>
2019-02-11 18:00:05 -0800

    Add perms

---
+++ auth/body.go
@@ -0,0 +1,22 @@
+package auth
+
+// Permission required to use the service
+type Permission int32
+
+const (
+	// NoPermission user is not allowed to use the service
+	NoPermission Permission = iota
+	// UserRegistration user is not allowed to use the service
+	UserRegistration
+	// User is only allowed to use services based on the ownership
+	User
+	// Admin is allowed to perform CRUD on everything
+	Admin
+)
+
+// Body contains the user's uuid, permission level, and expiration timestamp.
+type Body struct {
+	UUID                string
+	Permission          Permission
+	ExpirationTimestamp int64
+}
