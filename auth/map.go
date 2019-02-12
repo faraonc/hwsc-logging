35a39a1f4fdf67cf8d1ddf6fcdcfdb83131b96ab
Lisa Kim <lisakeem@outlook.com>
2019-02-11 21:54:37 -0800

    Remove double initialization of permission maps

--- auth/map.go
+++ auth/map.go
@@ -1,22 +1,19 @@
 package auth
 
 var (
-	PermissionStringMap map[Permission]string
-	PermissionEnumMap map[string]Permission
-)
-
-func init() {
-	PermissionStringMap = map[Permission]string {
-		NoPermission: "NO_PERM",
+	// PermissionStringMap maps Permission enum(int) values to its string values
+	PermissionStringMap = map[Permission]string{
+		NoPermission:     "NO_PERM",
 		UserRegistration: "USER_REGISTRATION",
-		User: "USER",
-		Admin: "ADMIN",
+		User:             "USER",
+		Admin:            "ADMIN",
 	}
 
-	PermissionEnumMap = map[string]Permission {
-		PermissionStringMap[NoPermission]: NoPermission,
+	// PermissionEnumMap maps string permission values to its enum(int) values
+	PermissionEnumMap = map[string]Permission{
+		PermissionStringMap[NoPermission]:     NoPermission,
 		PermissionStringMap[UserRegistration]: UserRegistration,
-		PermissionStringMap[User]: User,
-		PermissionStringMap[Admin]: Admin,
+		PermissionStringMap[User]:             User,
+		PermissionStringMap[Admin]:            Admin,
 	}
-}
+)
