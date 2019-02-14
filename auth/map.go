2913a483de7798c34b97a4522bd689af483814ee
Lisa Kim <43280172+kimlisa@users.noreply.github.com>
2019-02-13 18:17:20 -0800

    Make token and algorithm maps (#15)
    - fixed typos

--- auth/map.go
+++ auth/map.go
@@ -1,19 +1,40 @@
 package auth
 
+const (
+	strNoPerm           = "NO_PERM"
+	strUserRegistration = "USER_REGISTRATION"
+	strUser             = "USER"
+	strAdmin            = "ADMIN"
+)
+
 var (
 	// PermissionStringMap maps Permission enum(int) values to its string values
 	PermissionStringMap = map[Permission]string{
-		NoPermission:     "NO_PERM",
-		UserRegistration: "USER_REGISTRATION",
-		User:             "USER",
-		Admin:            "ADMIN",
+		NoPermission:     strNoPerm,
+		UserRegistration: strUserRegistration,
+		User:             strUser,
+		Admin:            strAdmin,
 	}
 
 	// PermissionEnumMap maps string permission values to its enum(int) values
 	PermissionEnumMap = map[string]Permission{
-		PermissionStringMap[NoPermission]:     NoPermission,
-		PermissionStringMap[UserRegistration]: UserRegistration,
-		PermissionStringMap[User]:             User,
-		PermissionStringMap[Admin]:            Admin,
+		strNoPerm:           NoPermission,
+		strUserRegistration: UserRegistration,
+		strUser:             User,
+		strAdmin:            Admin,
+	}
+
+	// AlgorithmMap maps permission level to algorithm type
+	AlgorithmMap = map[Permission]Algorithm{
+		NoPermission:     Hs256,
+		UserRegistration: Hs256,
+		User:             Hs256,
+		Admin:            Hs512,
+	}
+
+	// SignatureBytesMap maps algorithm to number of bytes needed for token size
+	SignatureBytesMap = map[Algorithm]int{
+		Hs256: 32,
+		Hs512: 64,
 	}
 )
