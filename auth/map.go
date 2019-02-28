e697282d20556e09d1f5155eba8f8415163adfd3
Lisa Kim <lisakeem@outlook.com>
2019-02-27 21:20:54 -0800

    Add token type map and algorithm map

--- auth/map.go
+++ auth/map.go
@@ -5,6 +5,11 @@ const (
 	strUserRegistration = "USER_REGISTRATION"
 	strUser             = "USER"
 	strAdmin            = "ADMIN"
+	strJWT              = "JWT"
+	strNoType           = "NO_TYPE"
+	strNoAlg            = "NO_ALG"
+	strHs256            = "HS256"
+	strHs512            = "HS512"
 	// SecretByteSize bytes used to generate secret key
 	SecretByteSize = 32
 )
@@ -32,5 +37,18 @@ var (
 		UserRegistration: Hs256,
 		User:             Hs256,
 		Admin:            Hs512,
+	}
+
+	// TokenTypeStringMap maps enum TokenType to its string value
+	TokenTypeStringMap = map[TokenType]string{
+		NoType: strNoType,
+		Jwt:    strJWT,
+	}
+
+	// AlgorithmStringMap maps enum Algorithm to its string value
+	AlgorithmStringMap = map[Algorithm]string{
+		NoAlg: strNoAlg,
+		Hs256: strHs256,
+		Hs512: strHs512,
 	}
 )
