3b2cb8a981c1837400c74b6be91b11f0a90ad6ba
CJ Faraon <faraonc@outlook.com>
2019-03-09 18:45:12 -0800

    Refactor hosts

---
+++ hosts/hosts.go
@@ -0,0 +1,30 @@
+package hosts
+
+// Host contains server configuration
+// `json:"address"` are key:value tags that can add meta information to structs
+// there can be json tags, yaml tags, xml, bson, protobuf, etc.
+// When json.Unmarshaling JSON file,
+// takes the "address" JSON property, and put it in the Address field of Host
+type Host struct {
+	Address string `json:"address"`
+	Port    string `json:"port"`
+	Network string `json:"network"`
+}
+
+// UserDBHost contains User database configurations
+type UserDBHost struct {
+	Host     string `json:"host"`
+	Name     string `json:"db"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Port     string `json:"port"`
+	SSLMode  string `json:"sslmode"`
+}
+
+// SMTPHost contains SMTP email configurations
+type SMTPHost struct {
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
