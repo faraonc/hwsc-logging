d325cdc90d1de59b659f189d504135dc5468b943
Lisa Kim <lisakeem@outlook.com>
2019-03-13 14:17:55 -0700

    Add JET token type and map (#28)

--- auth/map.go
+++ auth/map.go
@@ -6,6 +6,7 @@ const (
 	strUser             = "USER"
 	strAdmin            = "ADMIN"
 	strJWT              = "JWT"
+	strJET				= "JET"
 	strNoType           = "NO_TYPE"
 	strNoAlg            = "NO_ALG"
 	strHs256            = "HS256"
@@ -43,6 +44,7 @@ var (
 	TokenTypeStringMap = map[TokenType]string{
 		NoType: strNoType,
 		Jwt:    strJWT,
+		Jet: 	strJET,
 	}
 
 	// AlgorithmStringMap maps enum Algorithm to its string value
